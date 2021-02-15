894864a93c6febd857ba630adc2da0ed891e13ee
Michael Fuget <michaelfuget@gmail.com>
2021-02-15 00:35:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
 class Dog {
-  constructor(name, breed) {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+class Dog {
+  constructor(food, breed) {
     this.name = name;
     this.breed = breed;
   }
